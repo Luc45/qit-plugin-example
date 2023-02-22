c3772488ff9fe965c4de060476f93b81141d392c
Lucas Bustamante <lucasfbustamante@gmail.com>
2023-02-22 18:32:53 -0300

    Add PR comments, tweak example code

--- automatewoo/automatewoo.php
+++ automatewoo/automatewoo.php
@@ -5,20 +5,17 @@
  */
 
 add_action( 'init', static function() {
-    // This will be flagged by the Security scan as an unescaped output:
-    echo 'Unescaped output!';
 
     if ( isset( $_POST['foo'] ) ) {
-        // Flagged as unsanitized input.
+        // This will be flagged by the Security scan as unsanitized input.
         $foo = $_POST['foo'];
+
+        echo "Unescaped output! $foo";
     }
 
     if ( is_cart() ) {
         trigger_error( 'Simulating an error on the Cart pages.', E_USER_ERROR );
     }
-
-    trigger_error( 'Simulating a notice that happens on all requests on init.', E_NOTICE );
-    trigger_error( 'Simulating a notice that happens on all requests on init.', E_USER_NOTICE );
 } );
 
 trigger_error( 'Simulating a notice that happens on all requests.', E_USER_NOTICE );
